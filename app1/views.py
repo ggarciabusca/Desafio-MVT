4b319bdcef1c7f23cceee2eaa10c9856d5e05fd4
Gonzalo Garcia <ggarciabusca@gmail.com>
2022-11-12 11:32:31 -0300

    Ahora está casi listo

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from app1.models import Persona
+#from django.shortcuts import render
 # Create your views here.
+
+def listado_familiares(request): #función que devuelve el listado de familiares
+    
+    #leo todos los datos de la tabla app1-persona
+    #esto es una lista de instancias de la clase "Persona" en la base de datos
+    personas = Persona.objects.all() 
+
+# Armo la lista de familiares a devolver al formulario
+    cadena_respuesta = "<ul>"
+    for persona in personas:
+         cadena_respuesta += f"<li>{persona.nombre}, DNI: {persona.dni}, Fecha nacido:{persona.nacido} </li>"
+    cadena_respuesta += "</ul>"
+
+    return HttpResponse(cadena_respuesta)
